bf882ab845aae5743a1de99eacda860efb655bfd
solomon akinbiyi <akinbiyisolo22@gmail.com>
2024-05-03 08:32:05 +0100

    implemented logger

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,11 +1,21 @@
 import { Prop } from '@nestjs/mongoose';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
 
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
 
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
